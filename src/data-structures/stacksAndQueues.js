e3a86623b0c0df4792f45e8e524399ac24b808e1
Fox <taylorcasse@gmail.com>
2023-04-27 13:34:52 +0200

    finished stacks and queues

--- src/data-structures/stacksAndQueues.js
+++ src/data-structures/stacksAndQueues.js
@@ -1,4 +1,7 @@
-// Stacks
+// Stacks (array implimentation)
+// Linked list could be better since we don't need all the array methods 
+
+// No matter what, insertion and removal need to be O(1) for both stacks and queues
 
 class StackClass {
     #array = [];
@@ -37,9 +40,7 @@ class StackClass {
 class NewStack extends StackClass {
     constructor() {
         super();
-        return
     }
-
 }
 
 const StackContructor = () => {
@@ -104,6 +105,9 @@ const stackTests = () => {
 }
 // stackTests();
 
+// Stack (SLL implimentation with all methods constant time)
+
+
 class Node {
     constructor(value) {
         this.value = value;
@@ -111,41 +115,107 @@ class Node {
     }
 }
 
-class TestQueue {
-    constructor () {
-        this.head = null;
-        this.tail = null;
-        this.length = 0;
+class Stack {
+    // Preferred way of impimenting a stack with large data sets
+    constructor() {
+        this.first = null;
+        this.last = null;
+        this.size = 0;
     }
 
-    shift(value) {
-        const newNode = new Node(value);
-        if (!this.head) {
-            this.head = newNode;
-            this.tail = newNode;
+    push(val) {
+        const newNode = new Node(val);
+        if (!this.first) {
+            this.first = newNode;
+            this.last = newNode;
         } else {
-
-            let oldHead = this.head;
-            this.head = newNode;
-            this.head.next = oldHead;
-
-            if (this.length === 1) oldHead.prev = this.head;
+            const temp = this.first;
+            this.first = newNode;
+            this.first.next = temp;
         }
-
-        this.length++;
+        this.size++;
         return this;
     }
 
     pop() {
-        const poppedNode = this.tail;
+        if (!this.size) return undefined;
 
+        const temp = this.first;
+
+        if (this.size === 1) this.last = null;
+        
+        this.first = this.first.next;
+        this.size--;
+
+        return temp.value;
 
     }
-
-
 }
 
+// Queue (array implimentation)
+
+
+class ArrayQueue {
+    #array = [];
+    #length = 0;
+
+    add(value) {
+        this.#array.unshift(value);
+        this.#length++;
+        return this;
+    }
+
+    peek() {
+        return this.#array[this.#length - 1]
+    }
+
+    pop() {
+        if (!this.#length) return undefined;
+        this.#length--;
+        return this.#array.pop();
+    }
+}
 
 class Queue {
+    // Preferred way of implimenting a queue (SLL)
+    constructor() {
+        this.first = null;
+        this.last = null;
+        this.size = 0;
+    }
+
+    enqueue(value) {
+        const newNode = new Node(value);
+        if (!this.first) {
+            this.first = newNode;
+            this.last = newNode;
+        } else {
+            this.last.next = newNode;
+            this.last = newNode;
+        }
+
+        this.size++;
+        return this;
+    }
+
+    dequeue() {
+        if (!this.first) return undefined;
+        
+        const temp = this.first;
+        this.first = this.first.next;
+        if (this.size === 1) this.last = null;
+        this.size--;
+        return temp.value;
+    }
 
 }
+
+const testQueue = new Queue();
+testQueue.enqueue('Entry 1');
+testQueue.enqueue('Entry 2');
+testQueue.enqueue('Entry 3');
+console.log(testQueue)
+console.log(testQueue.dequeue())
+console.log(testQueue.dequeue())
+// console.log(testQueue.dequeue())
+console.log(testQueue)
---
+++ src/data-structures/test.js
@@ -0,0 +1,35 @@
+const myFactory = () => {
+    console.log('Factory:')
+    
+    let myProp = 0;
+
+    const increment = () => myProp++;
+    const getProp = () => myProp;
+
+    return {myProp, increment, getProp} 
+}
+
+
+class myClass {
+    constructor () {
+        console.log('Class:')
+        this.myProp = 0;
+    }
+
+    increment() {
+        return this.myProp++;
+    }
+
+    getProp() {
+        return this.myProp;
+    }
+
+
+}
+
+// const myObj = myFactory();
+const myObj = new myClass();
+
+myObj.increment()
+console.log('Method call: ' + myObj.getProp())
+console.log('Direct access: ' + myObj.myProp)
